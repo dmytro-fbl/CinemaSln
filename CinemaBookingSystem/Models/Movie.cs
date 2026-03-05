f107680170b7f1666136dec6703b97a1f9312877
dmytro-fbl <dmytrowork06@gmail.com>
2026-03-05 18:43:31 +0200

    completed project setup

---
+++ CinemaBookingSystem/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CinemaBookingSystem.Models
+{
+    public class Movie
+    {
+        public long? MovieID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+
+        [Column (TypeName ="decimal(8, 2)")]
+        public decimal TicketPrice { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
