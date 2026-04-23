86a3075999c8e939ace0282a24195e4bee074b61
dmytro-fbl <dmytrowork06@gmail.com>
2026-04-23 16:19:58 +0300

    added ViewModels for registration and login

---
+++ CinemaBookingSystem/Models/ViewModels/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaBookingSystem.Models.ViewModels
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Будь ласка, введіть ім'я користувача")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Будь ласка, введіть пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        public string? ReturnUrl { get; set; } = "/";
+    }
+}
