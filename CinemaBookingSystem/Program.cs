fa6131a0e7576066c22137cbcabcb76cc246f483
dmytro-fbl <dmytrowork06@gmail.com>
2026-04-23 15:52:06 +0300

    Configured Identity in the Program.cs file

--- CinemaBookingSystem/Program.cs
+++ CinemaBookingSystem/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using CinemaBookingSystem.Models;
 using CinemaBookingSystem.Models.Interface;
 using CinemaBookingSystem.Models.Repositories;
+using System.Numerics;
+using Microsoft.AspNetCore.Identity;
 
 namespace CinemaBookingSystem
 {
@@ -20,6 +22,21 @@ namespace CinemaBookingSystem
 
             builder.Services.AddScoped<ICinemaRepository, EFCinemaRepository>();
 
+            builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection")));
+
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.Password.RequiredLength = 8;
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+
+                options.User.RequireUniqueEmail = true;
+            })
+                .AddEntityFrameworkStores<AppIdentityDbContext>();
+
             builder.Services.AddDistributedMemoryCache();
             builder.Services.AddSession();
             builder.Services.AddRazorPages();
@@ -31,6 +48,9 @@ namespace CinemaBookingSystem
             app.UseRouting();
 
             app.UseSession();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.MapDefaultControllerRoute();
             
