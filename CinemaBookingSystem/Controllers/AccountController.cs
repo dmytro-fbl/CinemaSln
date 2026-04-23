58f4fb7717334359c5d830458638b67c2bbd2ab6
dmytro-fbl <dmytrowork06@gmail.com>
2026-04-23 16:55:00 +0300

    added a controller for registration, login and logout

---
+++ CinemaBookingSystem/Controllers/AccountController.cs
@@ -0,0 +1,91 @@
+﻿using CinemaBookingSystem.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaBookingSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<IdentityUser> userManager;
+        private SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userMgr, SignInManager<IdentityUser> signMgr)
+        {
+            userManager = userMgr;
+            signInManager = signMgr;
+        }
+
+        public ViewResult Register() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = new IdentityUser
+                {
+                    UserName = model.Name,
+                    Email = model.Email,
+                };
+
+                IdentityResult result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        public ViewResult Login( string? returnUrl)
+        {
+            return View(new LoginModel
+            {
+                Name = string.Empty,
+                Password = string.Empty,
+                ReturnUrl = returnUrl ?? "/"
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser? user = await userManager.FindByNameAsync(loginModel.Name);
+
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+
+                    if((await signInManager.PasswordSignInAsync(user, loginModel.Password, false, false)).Succeeded)
+                    {
+                        return Redirect(loginModel?.ReturnUrl ?? "/Admin");
+                    }
+                }
+                ModelState.AddModelError("", "Невірне ім'я або пароль");
+            }
+            return View(loginModel);
+
+        }
+
+        [Authorize]
+        public async Task<RedirectResult> logout(string returnUrl = "/")
+        {
+            await signInManager.SignOutAsync();
+            return Redirect(returnUrl);
+        }
+    }
+}
